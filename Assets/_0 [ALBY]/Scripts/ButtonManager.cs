8fb92a549ec673730195108e080e87a80cd0a868
Albytrosss <AlbatrossGames@pm.me>
2025-08-16 01:42:54 +1000

    Main Menu functionality added

--- Assets/_0 [ALBY]/Scripts/ButtonManager.cs
+++ Assets/_0 [ALBY]/Scripts/ButtonManager.cs
@@ -1,22 +1,57 @@
+using System;
 using UnityEngine;
-using DG.Tweening;
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-    public List<GameObject> buttons = new List<GameObject>();
-    public void HideAllButtons()
+    public enum MenuPage { Main, Options, Credits }
+    public MenuPage currentPage;
+    public GameObject menuMain;
+    public GameObject menuOptions;
+    public GameObject menuCredits;
+
+    public void Start()
     {
-        for (int i = 0; i < buttons.Count; i++)
-        {
-            HideButton(buttons[i], 0.2f * i);
-        }
+        currentPage = MenuPage.Main;
+    }
+    public void BXQuitApp() => Application.Quit();
+    public void BXLoadScene(string scene) => SceneManager.LoadScene(scene);
+    public void BXLoadMenu() => BXLoadScene("Menu");
+    public void BXLoadPage(int pageNum)
+    {
+        GetCurrentPage().SetActive(false);
+        SetPage(pageNum);
     }
 
-    private IEnumerator HideButton(GameObject btn, float sec)
+    private GameObject GetCurrentPage()
     {
-        yield return new WaitForSeconds(sec);
-        btn.transform.DOBlendableLocalMoveBy(new Vector3(-(Screen.width / 2), 0, 0), 0.5f);
+        switch (currentPage)
+        {
+            case MenuPage.Main:
+                return menuMain;
+            case MenuPage.Options:
+                return menuOptions;
+            case MenuPage.Credits:
+                return menuCredits;
+            default: return null;
+        }
+    }
+    private void SetPage(int page)
+    {
+        switch (page)
+        {
+            case 0:
+                menuMain.SetActive(true);
+                currentPage = MenuPage.Main;    // p.0
+                return;
+            case 1:
+                menuOptions.SetActive(true);
+                currentPage = MenuPage.Options; // p.1
+                return;
+            case 2:
+                menuCredits.SetActive(true);
+                currentPage = MenuPage.Credits; // p.2
+                return;
+        }
     }
 }
