546d12865e84964e6366ac9d0aaddf0761837ae6
Albytrosss <AlbatrossGames@pm.me>
2025-08-21 20:31:43 +1000

    Fixed many but not all menu bugs

---
+++ Assets/_0 [ALBY]/Scripts/TitleCode.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class TitleCode : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
+}
