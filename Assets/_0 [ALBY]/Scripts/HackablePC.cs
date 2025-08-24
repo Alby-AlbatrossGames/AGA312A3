4097c426e0c80d2e573400ad0e3d472608a52e12
Albytrosss <AlbatrossGames@pm.me>
2025-08-24 16:06:46 +1000

    Hackable PC core complete

--- Assets/_0 [ALBY]/Scripts/HackablePC.cs
+++ Assets/_0 [ALBY]/Scripts/HackablePC.cs
@@ -1,11 +1,27 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HackablePC : MonoBehaviour
 {
     private bool isHacked = false;
+    public UnityEvent OnHacked;
+    public GameObject screen;
+    public Material mat;
+    public Light L1;
+    [Header("Popup Text")]
+    public TMP_Text popup;
+    public TMP_Text key;
 
-    void Hack()
+    public void Hack()
     {
+        if (isHacked) return;
+
         isHacked = true;
+        OnHacked.Invoke();
+        screen.GetComponent<Renderer>().material = mat;
+        L1.color = Color.green;
+        popup.text = "Hacked";
+        key.color = Color.red;
     }
 }
